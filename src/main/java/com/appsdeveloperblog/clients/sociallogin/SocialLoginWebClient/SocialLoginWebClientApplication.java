f41ddbc0c2f8119baa7274b1a3b563e6f16e5d84
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2020-10-05 13:50:08 -0400

    Initial commit

---
+++ src/main/java/com/appsdeveloperblog/clients/sociallogin/SocialLoginWebClient/SocialLoginWebClientApplication.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.clients.sociallogin.SocialLoginWebClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialLoginWebClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialLoginWebClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/appsdeveloperblog/clients/sociallogin/SocialLoginWebClient/SocialLoginWebClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.clients.sociallogin.SocialLoginWebClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialLoginWebClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
