507f35d6363de2130acbc3274c1a8846b3f616a8
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2020-10-10 10:25:30 -0400

    Implemented OidcClientInitiatedLogoutSuccessHandler

--- src/main/java/com/appsdeveloperblog/clients/sociallogin/SocialLoginWebClient/security/WebSecurity.java
+++ src/main/java/com/appsdeveloperblog/clients/sociallogin/SocialLoginWebClient/security/WebSecurity.java
@@ -1,11 +1,17 @@
 package com.appsdeveloperblog.clients.sociallogin.SocialLoginWebClient.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	ClientRegistrationRepository clientRegistrationRepository;
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -16,11 +22,20 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 		.and().oauth2Login()
 		.and()
 		.logout()
-		.logoutSuccessUrl("/")
+		//.logoutSuccessUrl("/")
+		.logoutSuccessHandler(oidcLogoutSuccessHandler())
 		.invalidateHttpSession(true)
 		.clearAuthentication(true)
 		.deleteCookies("JSESSIONID");
 		
 	}
+	
+    private OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler() { 
+        OidcClientInitiatedLogoutSuccessHandler successHandler =
+        		new OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository);
+        successHandler.setPostLogoutRedirectUri("http://localhost:8080/");
+        return successHandler;
+    }
+	
 	
 }
