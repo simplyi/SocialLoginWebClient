ba8650536885dd61b5a11400570eb13e3f9a4255
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2023-08-18 11:44:51 -0400

    Updated to Spring Boot version 3.1.2

--- src/main/java/com/appsdeveloperblog/clients/sociallogin/SocialLoginWebClient/security/WebSecurity.java
+++ src/main/java/com/appsdeveloperblog/clients/sociallogin/SocialLoginWebClient/security/WebSecurity.java
@@ -1,41 +1,50 @@
 package com.appsdeveloperblog.clients.sociallogin.SocialLoginWebClient.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+@Configuration
 @EnableWebSecurity
-public class WebSecurity extends WebSecurityConfigurerAdapter {
-	
+public class WebSecurity {
+
 	@Autowired
 	ClientRegistrationRepository clientRegistrationRepository;
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		
-		http.authorizeRequests()
-		.antMatchers("/").permitAll()
-		.anyRequest().authenticated()
-		.and().oauth2Login()
-		.and()
-		.logout()
-		//.logoutSuccessUrl("/")
-		.logoutSuccessHandler(oidcLogoutSuccessHandler())
-		.invalidateHttpSession(true)
-		.clearAuthentication(true)
-		.deleteCookies("JSESSIONID");
-		
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+	    http.authorizeHttpRequests(authorizeRequests ->
+	            authorizeRequests
+	                .requestMatchers(new AntPathRequestMatcher("/"))
+	                .permitAll()
+	                .anyRequest()
+	                .authenticated()
+	    )
+	    .oauth2Login(Customizer.withDefaults())
+	    .logout(logout -> logout
+	        .logoutSuccessHandler(oidcLogoutSuccessHandler())
+	        .invalidateHttpSession(true)
+	        .clearAuthentication(true)
+	        .deleteCookies("JSESSIONID")
+	    );
+
+	    return http.build();
 	}
-	
-    private OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler() { 
-        OidcClientInitiatedLogoutSuccessHandler successHandler =
-        		new OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository);
-        successHandler.setPostLogoutRedirectUri("http://localhost:8080/");
-        return successHandler;
-    }
-	
-	
+
+
+	private OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler() {
+		OidcClientInitiatedLogoutSuccessHandler successHandler = new OidcClientInitiatedLogoutSuccessHandler(
+				clientRegistrationRepository);
+		successHandler.setPostLogoutRedirectUri("http://localhost:8080/");
+		return successHandler;
+	}
+
 }
